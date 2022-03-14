c6a5afea54e9d2378e52ba0d910844b10e1face4
Quentin Burg <qburg@cutii.io>
2022-03-14 22:07:51 +0100

    :sparkles: register professional

--- src/domain/ports/professional.ts
+++ src/domain/ports/professional.ts
@@ -1,5 +1,7 @@
+import { RegisterParams } from '../../shared.types';
 import { Professional } from '../entities/identity/professional';
 
 export interface ProfessionalPort {
   findAll(): Promise<Professional[]>;
+  create(args: RegisterParams): Promise<Professional>;
 }
--- src/domain/usecases/professional.ts
+++ src/domain/usecases/professional.ts
@@ -3,11 +3,11 @@ import { Professional } from '../entities/identity/professional';
 import { ProfessionalPort } from '../ports/professional';
 
 interface ProfessionalUseCases {
-  // register(args: RegisterParams): Professional;
+  register(args: RegisterParams): Promise<Professional>;
   findAllProfessionals(): Promise<Professional[]>;
 }
 
 export default (repo: ProfessionalPort): ProfessionalUseCases => ({
-  // register: args => {},
+  register: args => repo.create(args),
   findAllProfessionals: () => repo.findAll(),
 });
