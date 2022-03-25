e9b98d0a59835ddab5829ff4c4c205d025fd2e8b
Quentin Burg <qburg@cutii.io>
2022-03-25 22:41:25 +0100

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
+const cors = require('cors');
 import * as express from 'express';
 import { repository, sequelize } from './infra/sequelize';
 
 import routes from './routes/api';
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const options = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
